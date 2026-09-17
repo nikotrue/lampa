a8f39b32f277de6b990bd8107d3e9d361183abbf
niko <koluhaop.4@gmail.com>
2026-09-17 21:54:45 +0300

    test logging

--- TraktTV/trakttv.js
+++ TraktTV/trakttv.js
@@ -1491,12 +1491,12 @@
                                 logging = Lampa.Storage.field('trakt_enable_logging');
                                 clearAuthStorage();
                             }
-                            if (logging) logWarn('refreshTokens failed', //{
-                                //reason: reason,
-                                //error: error,
+                            if (logging) logWarn('refreshTokens failed', {
+                                reason: reason,
+                                error: error,
                                 //nikotrue
                                 //redirect_uri: redirect_uri
-                            //},
+                            },
                             {
                                 debugOnly: true
                             });
