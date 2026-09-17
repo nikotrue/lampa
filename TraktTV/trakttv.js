95dd7a3ef42c7a91a5d74ae0c93f940ad3e9a6ff
niko <koluhaop.4@gmail.com>
2026-09-17 22:42:43 +0300

    test logging

--- TraktTV/trakttv.js
+++ TraktTV/trakttv.js
@@ -7686,8 +7686,14 @@
                     Lampa.Storage.set('trakt_token_created_at', null);
                     Lampa.Storage.set('trakt_token_expires_in', null);
                     Lampa.Storage.set('trakt_token_expires_at', null);
-                    Lampa.Storage.set('trakt_active_device_auth', false);
+                    Lampa.Storage.set('trakt_active_device_auth', null);
                     Lampa.Storage.set('trakt_active_device_auth_started_at', null);
+                    Lampa.Storage.set('trakt_hash_meta_cache', null);
+                    Lampa.Storage.set('trakt_completion_cache', null);
+                    Lampa.Storage.set('trakt_last_card', null);
+                    Lampa.Storage.set('trakttv_cached_upnext', null);
+                    Lampa.Storage.set('trakttv_cached_recommendations', null);
+                    Lampa.Storage.set('trakt_user_vip', null);
                 }
                 Lampa.Bell.push({
                     text: Lampa.Lang.translate('trakttvFullClearNoty')
